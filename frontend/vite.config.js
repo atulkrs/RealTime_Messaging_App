ad11a834d9b86a97dcd9ce9550bc725ca709b3a7
atulkrs <atulkrs26@gmail.com>
2025-09-14 19:22:00 +0530

    third

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,14 @@
-// vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // or whatever your frontend port is
+    port: 5173, 
     proxy: {
       '/socket.io': {
-        target: 'http://localhost:5000', // your backend
+        target: 'http://localhost:5000',
         ws: true,
       },
       '/api': {
